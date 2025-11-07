8f4814f4ac6f9aa8b95b80ce2091322c9a310e14
Jared Dingman <jareddingman@gmail.com>
2025-11-07 14:13:40 -0600

    Update LoadModel.py

--- LoadModel.py
+++ LoadModel.py
@@ -1,21 +1,115 @@
-import os
-import xml.etree.ElementTree as ET
-from PIL import Image
+import pandas as pd
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+# For visualizing
+import plotly.express as px
+
+# For model building
 import torch
-from torch.utils.data import Dataset, DataLoader, random_split
-from torchvision import datasets, transforms
+import torch.nn as nn
+import torch.nn.functional as F
 
-#Specify our path
-PATH = "model.pt"
+class FirstNet(nn.Module):
+    def __init__(self):
+      # We define the components of our model here
+      super(FirstNet, self).__init__()
+      # Function to flatten our image
+      self.flatten = nn.Flatten()
+      # Create the sequence of our network
+      self.linear_relu_model = nn.Sequential(
+            # Add a linear output layer w/ 10 perceptrons
+            nn.LazyLinear(10),
+        )
 
-# # Create a new "blank" model to load our information into
+    def forward(self, x):
+      # We construct the sequencing of our model here
+      x = self.flatten(x)
+      # Pass flattened images through our sequence
+      output = self.linear_relu_model(x)
+
+      # Return the evaluations of our ten 
+      #   classes as a 10-dimensional vector
+      return output
+
+# Create an instance of our model
 model = FirstNet()
 
-# # Recreate our optimizer
-optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+# Define some training parameters
+learning_rate = 1e-2
+batch_size = 16
+epochs = 10
 
-# # Load back all of our data from the file
-checkpoint = torch.load(PATH)
-model.load_state_dict(checkpoint['model_state_dict'])
-optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-EPOCH = checkpoint['epoch']
+# Define our loss function
+#   This one works for multiclass problems
+loss_fn = nn.CrossEntropyLoss()
+# Build our optimizer with the parameters from
+#   the model we defined, and the learning rate
+#   that we picked
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    # Set the model to training mode
+    # important for batch normalization and dropout layers
+    # Unnecessary in this situation but added for best practices
+    model.train()
+    # Loop over batches via the dataloader
+    for batch, (X, y) in enumerate(dataloader):
+        # Compute prediction and loss
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation and looking for improved gradients
+        loss.backward()
+        optimizer.step()
+        # Zeroing out the gradient (otherwise they are summed)
+        #   in preparation for next round
+        optimizer.zero_grad()
+
+        # Print progress update every few loops
+        if batch % 10 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+def test_loop(dataloader, model, loss_fn):
+    # Set the model to evaluation mode
+    # important for batch normalization and dropout layers
+    # Unnecessary in this situation but added for best practices
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+    print("Running test loop...")  # indicator that testing has started
+
+    # Evaluating the model with torch.no_grad() ensures
+    # that no gradients are computed during test mode
+    # also serves to reduce unnecessary gradient computations
+    # and memory usage for tensors with requires_grad=True
+    with torch.no_grad():
+        for batch, (X, y) in enumerate(dataloader):
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+            if batch % 10 == 0:
+                print(f"  [Batch {batch+1}/{num_batches}] running...")
+
+    # Printing some output after a testing round
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {
+        (100*correct):>0.1f}%, Avg loss: {
+            test_loss:>8f} \n")
+    
+# Need to repeat the training process for each epoch.
+#   In each epoch, the model will eventually see EVERY
+#   observations in the data
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(train_loader, model, loss_fn, optimizer)
+    test_loop(val_loader, model, loss_fn=loss_fn)
+print("Done!")
+
+'''
+NOTE: this model will get 100 percent accuracy because of:
+
+label = sample["labels"][0] (see fullAssingment.py)
+'''
